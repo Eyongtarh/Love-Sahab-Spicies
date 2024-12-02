18af2823e7b1962081354d7d63cf28f2949bcea9
Augustine Eyong <eyongtarhb@yahoo.co.uk>
2024-12-02 22:53:22 +0000

    creat worksheet update function and add sales update function to it

--- run.py
+++ run.py
@@ -51,10 +51,22 @@ def validate_data(values):
 
     return True
 
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet... \n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 def main():
     """
     Run all program functions
     """
     data = get_sales_data()
+    sales_data = [int(num) for num in data] 
+    update_worksheet(sales_data, "sales")
 
 main()
